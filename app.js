8ad4bb7457532a7c828959314885f56cd391356b
Deeter <deeter.cesler@gmail.com>
2018-10-01 13:52:15 -0600

    added sound effects

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// =============================================================
+// ============ STARTER FUNCTIONS, DECLARATIONS  ===============
+// =============================================================
+
 const buildGrid = (height, width) => {
     maxHeight = height - 1;
     maxWidth = width - 1;
@@ -15,20 +19,32 @@ const buildGrid = (height, width) => {
     // width = number of columns per row
 }
 
+// ====================
+// INITIAL DECLARATIONS
+// ====================
+
 let maxHeight;
 let maxWidth;
-
-
-
-buildGrid(20,20);
 
 let score = 0;
 let level = 1;
 let enemiesKilled = 0;
 let newEnemyPosition;
+let escapePosition;
+
+// ============
+// UNUSED TIMER
+// ============
+
 // $("body").append(`<div class='score'>${score}</div>`)
 // let timer = 20;
 // $(".timer h2").html(`${timer}`);
+
+
+// =============================================================
+// ====================== MAKE HERO  ===========================
+// =============================================================
+
 
 const hero = {
     x: 0,
@@ -44,19 +60,14 @@ const hero = {
             }
             else{
                 $(`${swordLocation} #coin`).remove();
-                $(`${swordLocation} .breakable-wall`).remove();
+                if($(`${swordLocation} .breakable-wall`).length){
+                    $("body").append("<audio autoplay><source src='sounds/cave-in-2.wav'></audio>")
+                    $(`${swordLocation} .breakable-wall`).remove();
+                }
                 if($(`${swordLocation} .enemy`).length){
+                    $("body").append("<audio autoplay><source src='sounds/swoosh.mp3'></audio>")
                     $(`${swordLocation} .enemy`).remove();
                     enemiesKilled++;
-                    // $(`${swordLocation} #baddo`).remove();
-                    // function stopMoving () {
-                    //     clearInterval(baddo1move)   
-                    // };
-                    // stopMoving();
-                    // how to clear the interval of an object in an element
-                    // how to call the object attached to an element
-                    // maybe I give the enemy a unique ID that changes as they move
-                    // and then I use that unique ID to clear the interval
                 }
                 if($(`#row${hero.y}column${hero.x-1} .holder .breakable-wall`).length || $(`#row${hero.y}column${hero.x-1} .holder .wall`).length){
                     console.log("that's a wall");
@@ -64,6 +75,7 @@ const hero = {
                 // this if check exists so that if someone attacks twice really quickly,
                 // the sword won't show up twice in the same block
                 else if($(`${swordLocation} #sword`).length === 0){
+                    $("body").append("<audio autoplay><source src='sounds/swoosh.mp3'></audio>")
                     $(swordLocation).append("<img id='sword' src='img/sword.png'>");
                     // sword in front of you
                     setTimeout(function(){
@@ -98,13 +110,6 @@ const hero = {
         }
     }
 }
-
-// start 
-
-let newPosition = "#row0column0";
-let coinPosition;
-
-$(`${newPosition} .holder`).append("<div id='snake-head'><img src='img/arrow-right.jpg'></div>");
 
 // move a block from grid point to grid point
 const moveRight = () => {
@@ -206,22 +211,11 @@ const controls = () => {
       
 controls();
 
-// placing coin on the map
-
-$("#row1column9 .holder").append("<div class='coin'></div>")
-$("#row1column11 .holder").append("<div class='coin'></div>")
-$("#row1column13 .holder").append("<div class='coin'></div>")
-
-// placing walls on the map
-
-// $("#row7column3 .holder").append("<div class='breakable-wall'></div>")
-
-coinPosition = $(".coin").parent().parent().attr("id");
-
 const gameOver = () => {
     $(".grid").empty();
     $(".grid").append("<br />")
     $(".grid").append("<h1>GAME OVER</h1>");
+    $(".grid").append("<br />")
     $(".grid").append(`<h3>You got to level ${level}.</h3>`);
     $(".grid").append(`<h3>You killed ${enemiesKilled} enemies.</h3>`);
     $(".grid").append(`<h3>You got a score of ${score}.</h3>`);
@@ -229,6 +223,7 @@ const gameOver = () => {
 
 const coinCheck = () => {
     if($("#snake-head").siblings().is(".coin")){
+        $("body").append("<audio autoplay><source src='sounds/ting.wav'></audio>")
         score++;
         $(`${newPosition} .coin`).remove();
     }
@@ -236,143 +231,36 @@ const coinCheck = () => {
 
 const enemyCheck = () => {
     if($("#snake-head").siblings().is(".enemy")){
-    console.log("samesies");
+        $("body").append("<audio autoplay><source src='sounds/error.wav'></audio>")   
         gameOver();
     }
 };
-    
-let escapePosition;
 
 const escapeCheck = () => {
     if(escapePosition === newPosition + " .holder"){
+        $("body").append("<audio autoplay><source src='sounds/ta-da.wav'></audio>")
         level++;
         $(".escape").remove();
-        stage2();
-        // $(".score").empty();
-        // $(".score").html(`<div class='score'>${score}</score>`);
+        if(level === 2){
+            stage2();
+        }
+        if(level === 3){
+            stage3();
+        }
     }
 };
 
-// move a snake around the screen
-// Make it die if it hits a wall
-// Make it die if it hits itself
-// make it grow if it eats a coin
-// Figure out a way to vary the grid sizes
+// =============================================================
+// ==================== USER INSTRUCTIONS ======================
+// =============================================================
+
 
 $("body").prepend("<h2>Use W, A, S, and D to move. <br/> Press space to attack. <br /> <br/> Collect coins, avoid (or fight) enemies, <br/> and escape through the purple door!</h2>")
 
-const stage1 = () => {
-    // top row
-    for(let i=0; i<17; i++){
-        $(`#row3column${i} .holder`).append("<div class='wall'></div>")
-    };
-    // next row 
-    for(let i=maxWidth; i>2; i--){
-        $(`#row6column${i} .holder`).append("<div class='wall'></div>")
-    };
-    // wall along left
-    for(let i=7; i<17; i++){
-        $(`#row${i}column3 .holder`).append("<div class='wall'></div>")
-    };
-    // breakable walls
-    for(let i=0; i<3; i++){
-        $(`#row10column${i} .holder`).append("<div class='breakable-wall'></div>")
-    }
-    // walled-in escape - top wall
-    for(let i=16; i<21; i++){
-        $(`#row16column${i} .holder`).append("<div class='breakable-wall'></div>")
-    }
-    // walled-in escape - side wall
-    for(let i=17; i<21; i++){
-        $(`#row${i}column16 .holder`).append("<div class='breakable-wall'></div>")
-    }
-    // escape door
-    escapePosition = "#row18column18 .holder";
-    $(escapePosition).append("<div class='escape'></div>")
-};
 
-const clearStage = () => {
-    $(".grid").empty();
-    buildGrid(20,20);
-    hero.x = 0;
-    hero.y = 0;
-    newPosition = "#row0column0";
-    $(`${newPosition} .holder`).append("<div id='snake-head'><img src='img/arrow-right.jpg'></div>");
-    escapePosition = "#row15column15 .holder";
-};
-
-const stage2 = ()=>{
-    console.log("NEXT LEVEL");
-    clearStage();
-    // top row
-    // for(let i=0; i<17; i++){
-    //     $(`#row${i}column3 .holder`).append("<div class='wall'></div>")
-    // };
-    // // next row 
-    // for(let i=maxWidth; i>2; i--){
-    //     $(`#row${i}column6 .holder`).append("<div class='wall'></div>")
-    // };
-    // // wall along left
-    // for(let i=7; i<17; i++){
-    //     $(`#row3column${i} .holder`).append("<div class='wall'></div>")
-    // };
-    // // breakable walls
-    // for(let i=0; i<3; i++){
-    //     $(`#row${i}column10 .holder`).append("<div class='breakable-wall'></div>")
-    // }
-    // // walls
-    // for(let i=19; i>8; i--){
-    //     $(`#row6column${i} .holder`).append("<div class='wall'></div>")
-    // }
-    for(let i=0; i<8; i++){
-        $(`#row8column${i} .holder`).append("<div class='wall'></div>")
-    }
-    for(let i=11; i<21; i++){
-        $(`#row8column${i} .holder`).append("<div class='wall'></div>")
-    }
-    // breakable cage
-        for(let i=7; i<11; i++){
-            $(`#row2column${i} .holder`).append("<div class='breakable-wall'></div>")
-        }
-        for(let i=2; i<6; i++){
-            $(`#row${i}column11 .holder`).append("<div class='breakable-wall'></div>")
-        }
-        for(let i=3; i<6; i++){
-            $(`#row${i}column7 .holder`).append("<div class='breakable-wall'></div>")
-        }
-        for(let i=7; i<12; i++){
-            $(`#row6column${i} .holder`).append("<div class='breakable-wall'></div>")
-        }
-    // walled-in escape - top wall
-    for(let i=16; i<21; i++){
-        $(`#row16column${i} .holder`).append("<div class='breakable-wall'></div>")
-    }
-    // walled-in escape - side wall
-    for(let i=17; i<21; i++){
-        $(`#row${i}column16 .holder`).append("<div class='breakable-wall'></div>")
-    }
-
-    // make enemy
-    const baddo4 = new Enemy(9,4);
-    baddo4move = setInterval(function(){
-    baddo4.move();
-    if($(`#row${baddo4.y}column${baddo4.x} .enemy`).length > 0){
-        console.log("baddo4 alive")
-        baddo4move;
-    }
-}, 1500);
-    // $(`#row12column8 .holder`).append("<div class='enemy'></div>")
-    // baddo1move = setInterval(function(){
-    //     baddo1.move();
-    // }, 2000);
-};
-
-stage1();
-
-
-// ==========================================================================
-// ============================ Making enemies ==============================
-// ==========================================================================
+// =============================================================
+// ======================== ENEMY CLASS ========================
+// =============================================================
 
 
 class Enemy {
@@ -436,41 +324,162 @@ class Enemy {
 
 // the block of code for calling a new enemy, 
 // and the function to let him move (if he's still alive)
-const baddo1 = new Enemy(9,8);
-baddo1move = setInterval(function(){
-    baddo1.move();
-    if($(`#row${baddo1.y}column${baddo1.x} .enemy`).length > 0){
-        baddo1move;
+
+
+
+
+
+// =========================================================
+// =================== MAKING STAGES =======================
+// =========================================================
+
+const clearStage = () => {
+    $(".grid").empty();
+    buildGrid(20,20);
+    hero.x = 0;
+    hero.y = 0;
+    newPosition = "#row0column0";
+    $(`${newPosition} .holder`).append("<div id='snake-head'><img src='img/arrow-right.jpg'></div>");
+};
+
+const stage1 = () => {
+    // building grid
+    buildGrid(20,20);
+
+    // establish hero
+    let newPosition = "#row0column0";
+    $(`${newPosition} .holder`).append("<div id='snake-head'><img src='img/arrow-right.jpg'></div>");
+
+
+    // making enemies
+    const stage1enemies = () => {
+        const baddo1 = new Enemy(9,8);
+        baddo1move = setInterval(function(){
+            baddo1.move();
+            if($(`#row${baddo1.y}column${baddo1.x} .enemy`).length > 0){
+                baddo1move;
+            }
+        }, 2000);
+    
+        const baddo2 = new Enemy(9,10);
+        baddo2move = setInterval(function(){
+            baddo2.move();
+            if($(`#row${baddo2.y}column${baddo2.x} .enemy`).length > 0){
+                baddo2move;
+            }
+        }, 1500);
+    
+        const baddo3 = new Enemy(12,10);
+        baddo3move = setInterval(function(){
+            baddo3.move();
+            if($(`#row${baddo3.y}column${baddo3.x} .enemy`).length > 0){
+                baddo3move;
+            }
+        }, 1500);
     }
-}, 2000);
+    
+    stage1enemies();
 
-const baddo2 = new Enemy(9,10);
-baddo2move = setInterval(function(){
-    baddo2.move();
-    if($(`#row${baddo2.y}column${baddo2.x} .enemy`).length > 0){
-        baddo2move;
+
+    // placing coins on the map
+
+    $("#row1column9 .holder").append("<div class='coin'></div>")
+    $("#row1column11 .holder").append("<div class='coin'></div>")
+    $("#row1column13 .holder").append("<div class='coin'></div>")
+
+    // top row
+    for(let i=0; i<17; i++){
+        $(`#row3column${i} .holder`).append("<div class='wall'></div>")
+    };
+    // next row 
+    for(let i=maxWidth; i>2; i--){
+        $(`#row6column${i} .holder`).append("<div class='wall'></div>")
+    };
+    // wall along left
+    for(let i=7; i<17; i++){
+        $(`#row${i}column3 .holder`).append("<div class='wall'></div>")
+    };
+    // breakable walls
+    for(let i=0; i<3; i++){
+        $(`#row10column${i} .holder`).append("<div class='breakable-wall'></div>")
     }
-}, 1500);
-
-const baddo3 = new Enemy(12,10);
-baddo3move = setInterval(function(){
-    baddo3.move();
-    if($(`#row${baddo3.y}column${baddo3.x} .enemy`).length > 0){
-        baddo3move;
+    // walled-in escape - top wall
+    for(let i=16; i<21; i++){
+        $(`#row16column${i} .holder`).append("<div class='breakable-wall'></div>")
     }
-}, 1500);
+    // walled-in escape - side wall
+    for(let i=17; i<21; i++){
+        $(`#row${i}column16 .holder`).append("<div class='breakable-wall'></div>")
+    }
+    // escape door
+    escapePosition = "#row18column18 .holder";
+    $(escapePosition).append("<div class='escape'></div>")
+};
 
+stage1();
 
-// const move = () => {
-//     findRandomDirection();
-//     setOrientation();
-//     // move one forward in the direction it's facing
-//     $(".enemy").remove();
-//     const newEnemyPosition = "#row" + this.y + "column" + this.x;
-//     $(`${newEnemyPosition} .holder`).append("<div class='enemy'></div>");
-//     // code below removes the sword so that if you walk forward into it, 
-//     // it doesn't fuck up the whole grid
-//     if($(`${newPosition} #sword`).length > 0){
-//         $(`#sword`).remove();
-//     }
-// }
+const stage2 = ()=>{
+    console.log("NEXT LEVEL");
+    clearStage();
+    // top row
+    // for(let i=0; i<17; i++){
+    //     $(`#row${i}column3 .holder`).append("<div class='wall'></div>")
+    // };
+    // // next row 
+    // for(let i=maxWidth; i>2; i--){
+    //     $(`#row${i}column6 .holder`).append("<div class='wall'></div>")
+    // };
+    // // wall along left
+    // for(let i=7; i<17; i++){
+    //     $(`#row3column${i} .holder`).append("<div class='wall'></div>")
+    // };
+    // // breakable walls
+    // for(let i=0; i<3; i++){
+    //     $(`#row${i}column10 .holder`).append("<div class='breakable-wall'></div>")
+    // }
+    // // walls
+    // for(let i=19; i>8; i--){
+    //     $(`#row6column${i} .holder`).append("<div class='wall'></div>")
+    // }
+    for(let i=0; i<8; i++){
+        $(`#row8column${i} .holder`).append("<div class='wall'></div>")
+    }
+    for(let i=11; i<21; i++){
+        $(`#row8column${i} .holder`).append("<div class='wall'></div>")
+    }
+    // breakable cage
+        for(let i=7; i<11; i++){
+            $(`#row2column${i} .holder`).append("<div class='breakable-wall'></div>")
+        }
+        for(let i=2; i<6; i++){
+            $(`#row${i}column11 .holder`).append("<div class='breakable-wall'></div>")
+        }
+        for(let i=3; i<6; i++){
+            $(`#row${i}column7 .holder`).append("<div class='breakable-wall'></div>")
+        }
+        for(let i=7; i<12; i++){
+            $(`#row6column${i} .holder`).append("<div class='breakable-wall'></div>")
+        }
+    // walled-in escape - top wall
+    for(let i=16; i<21; i++){
+        $(`#row16column${i} .holder`).append("<div class='breakable-wall'></div>")
+    }
+    // walled-in escape - side wall
+    for(let i=17; i<21; i++){
+        $(`#row${i}column16 .holder`).append("<div class='breakable-wall'></div>")
+    }
+
+    // make enemy
+    const baddo4 = new Enemy(9,4);
+    baddo4move = setInterval(function(){
+    baddo4.move();
+    if($(`#row${baddo4.y}column${baddo4.x} .enemy`).length > 0){
+        console.log("baddo4 alive")
+        baddo4move;
+    }
+    }, 1500);
+
+    // escape door
+    escapePosition = "#row18column18 .holder";
+    $(escapePosition).append("<div class='escape'></div>")
+};
